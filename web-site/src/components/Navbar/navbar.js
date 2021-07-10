da4ec78ed3ba575ca25df54dbf6c437eef05f5b5
JarredGriffith <Jarredgriffith29@gmail.com>
2021-07-10 14:07:08 -0700

    adding hamburger menu and mobile function.

--- web-site/src/components/Navbar/navbar.js
+++ web-site/src/components/Navbar/navbar.js
@@ -2,19 +2,24 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Nav from 'react-bootstrap/esm/Nav';
 import Navbar from 'react-bootstrap/Navbar'
-import "./NavBar.css"
+import "./styles/MainNav.css"
 
 
 function NavBar() {
     return (
-        <Navbar >
+        <Navbar className="nav">
         <Navbar.Brand href="/">logo</Navbar.Brand>
-        <div className="NavBarText">
+        <div className="NavBarText hide_for_mobile">
         <Nav className="ml-auto" >
         <Nav.Link href="/">Home</Nav.Link>
           <Nav.Link href="/Projects">Projects</Nav.Link>
           <Nav.Link href="/About">About</Nav.Link>
         </Nav> 
+        </div>
+        <div className="Hambuger hide_for_desktop">
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
       </Navbar>
     ); 
